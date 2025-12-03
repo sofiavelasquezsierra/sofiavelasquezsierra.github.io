d3372860aefb79d15e522c8eae6166cd90a87dc1
Sofia Velasquez <sofia.velasquezsierra@mail.mcgill.ca>
2025-12-02 19:51:53 -0500

    new websitegit add .

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import IslandsCanvas from "./Islands";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, IslandsCanvas, StarsCanvas };
